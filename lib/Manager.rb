66008c67a7c60bacb61a36f407dd0354df55740f
github username <github email address>
2019-08-30 14:01:31 -0500

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,37 @@
 class Manager
 
+    attr_reader :name, :department, :age
+    @@all = []
+    @@departments = []
+    @@ages = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+        @@departments << department
+        @@ages << age
+    end
+
+    def employees
+        e = Employee.all.select {|employee| employee.manager == self}
+    end
+
+    def self.all
+        @@all
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self)
+    end
+
+    def self.all_departments
+        @@departments
+    end
+
+    def self.average_age
+        @@ages.reduce(:+) / @@ages.count
+    end
+
 end
--- run.rb
+++ run.rb
@@ -2,9 +2,13 @@ require_relative "lib/Manager"
 require_relative "lib/Employee"
 require 'pry'
 
-
-#Test your code here
-
+m1 = Manager.new("Michael", "People", 50)
+m2 = Manager.new("Jan", "Corporate", 51)
+m3 = Manager.new("Jim", "Paper", 35)
+e1 = Employee.new("Dwight",80000, m1)
+e2 = Employee.new("Pam",40000, m3)
+e3 = Employee.new("Toby",40999, m2)
+e4 = Employee.new("Oscar",50001, m2)
 
 binding.pry
 puts "done"
